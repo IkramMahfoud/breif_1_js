33e331c4bece27fe5ad724d413a56e6e10c5d9a5
IkramMahfoud <ikrammahfoud9nemo@gmail.com>
2023-09-22 17:36:43 +0100

    code js

--- game.js
+++ game.js
@@ -1,39 +1,57 @@
 const holes = document.getElementsByClassName("hole");
 
+hungryMole= '<img src="./hungry.png" alt="mole" class="mole"">'
+sadMole= '<img src="./sad.png" alt="mole" class="mole" ">'
+leavingMole = '<img data-index="8" src="./leaving.png" alt="mole" class="mole">'
 feedingMole = '<img data-index="6" src="./fed.png" alt="mole" class="mole">'
 happyMole = '<img data-index="6" src="./fed.png" alt="mole" class="mole">'
-leavingMole = '<img data-index="8" src="./leaving.png" alt="mole" class="mole">'
+let statusMole = "hungry";
 
-function hungryMole(u) {
-  return '<img data-index="'+u+'" src="./hungry.png" alt="mole" class="mole" onclick="click(this)">'
-}
-// console.log(t);
 function getRandom()
 {
   return Math.floor(Math.random() * 7);
 }
 
-setInterval(function ()
-{
-  let n = getRandom()
-  console.log(n);
-  console.log(holes[n]);
-  holes[n].innerHTML = hungryMole(n);
-  setTimeout(function(){
-    holes[n].removeChild(holes[n].firstChild);
-  },2000)
-}, 1000);
+setInterval(function () {
+  const n = getRandom();
+  const hole = holes[n];
 
-function click(mole)
-{
-  u = mole.data_index;
-  if(mole.src === hungryMole){
-    holes[u].removeChild(holes[u].firstChild);
-    console.log();
-    holes[u].innerHTML = happyMole;
-      setTimeout(function () {
-        holes[u].removeChild(holes[u].firstChild);
-        holes[u].innerHTML = leavingMole;
-      },500)
-  }
-}
+  console.log(n);
+  console.log(hole);
+
+  hole.innerHTML = hungryMole;
+
+  hole.querySelector(".mole").addEventListener("click", function () {
+    statusMole = "happy"
+    hole.innerHTML = happyMole;
+  } )
+
+
+
+  setTimeout(function () {
+    if (statusMole == "hungry") { hole.innerHTML = sadMole};
+  }, 1400);
+
+  setTimeout(function () {
+      hole.innerHTML = leavingMole;
+  }, 2000);
+  setTimeout(() => {
+    hole.querySelector(".mole").remove();
+  }, 2600)
+
+
+}, 3000);
+
+
+// function click(mole)
+// {
+//   if(mole.src === hungryMole){
+//     holes[u].removeChild(holes[u].firstChild);
+//     console.log();
+//     holes[u].innerHTML = happyMole;
+//       setTimeout(function () {
+//         holes[u].removeChild(holes[u].firstChild);
+//         holes[u].innerHTML = leavingMole;
+//       },500)
+//   }
+// }
